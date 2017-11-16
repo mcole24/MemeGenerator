84affe0d434debac469c0f70a49bbe3014229c42
Matthew Cole <mattaecole@gmail.com>
2017-11-16 01:02:09 -0500

    Created app with gallery functionality. Commit is before attempt to rearrange layout

---
+++ app/src/main/java/com/rss/matthewcole/memegenerator/MainActivity.java
@@ -0,0 +1,97 @@
+package com.rss.matthewcole.memegenerator;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int PHOTO_INTENT_REQUEST_CODE = 10;
+    public static final int EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE = 20;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button shareButton = (Button)findViewById(R.id.button_share);
+        shareButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                sharePhoto();
+            }
+        });
+
+    }
+
+    private void sharePhoto() {
+
+    }
+
+
+    public void pickPhotoFromGallery(View v) {
+        requestPermission();
+    }
+
+    private void createPhotoIntent() {
+        Intent photoIntent = new Intent(Intent.ACTION_PICK);
+        File photoDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        Uri photoUri = Uri.parse(photoDirectory.getPath());
+        photoIntent.setDataAndType(photoUri, "image/*");
+        startActivityForResult(photoIntent, PHOTO_INTENT_REQUEST_CODE);
+    }
+
+    private void requestPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE);
+        } else {
+            createPhotoIntent();
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch(requestCode) {
+            case EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    createPhotoIntent();
+                } else {
+                    Toast.makeText(this, "Gallery Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == PHOTO_INTENT_REQUEST_CODE) {
+                Uri photoUri = data.getData();
+                ImageView imageView = (ImageView)findViewById(R.id.image_view_meme);
+                Picasso.with(this).load(photoUri).into(imageView);
+            }
+        }
+    }
+
+
+}
